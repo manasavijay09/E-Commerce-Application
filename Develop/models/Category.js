d05813aec204da3b56a52cee5b08b22b280a0180
manasavijay09 <manasa.vijay09@gmail.com>
2024-02-06 21:34:07 -0600

    created product model rules in product.js file

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,31 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // database model category columns requirements:
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-  
-      category_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
